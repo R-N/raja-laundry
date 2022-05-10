8c78ae23a7a9f0fddf638eae5d16ce124702dbd6
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2022-05-11 00:21:19 +0700

    Laragon
    - Change database name to raja-laundry
    - Dynamic base url and cookie path
    - Fix timezone error

--- dump.sql
+++ dump.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 10.1.34-MariaDB : Database - rajalaundry
+MySQL - 10.1.34-MariaDB : Database - raja-laundry
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.1.34-MariaDB : Database - rajalaundry
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`rajalaundry` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`raja-laundry` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `rajalaundry`;
+USE `raja-laundry`;
 
 /*Table structure for table `customer` */
 
