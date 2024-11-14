98ed43e6ad17886c254ab6763a7e0b6e3d91f2b7
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-14 23:00:18 +0700

    Update Laundry.php

--- application/models/Laundry.php
+++ application/models/Laundry.php
@@ -39,11 +39,8 @@ class Laundry extends CI_Model {
 
         // Step 1: Add schema name to tables in FROM, JOIN, LEFT JOIN, RIGHT JOIN clauses (only once per table name)
         $query = preg_replace_callback('/\b(FROM|JOIN|LEFT\s+JOIN|RIGHT\s+JOIN)\s+([a-zA-Z0-9_]+)(?=\s*(?:\s|\b|\s+ON|\s+WHERE))/i', function($matches) use ($schema) {
-            echo $matches[2];
             return $matches[1] . ' "' . $schema . '".' . $matches[2];
         }, $query);
-        echo $query;
-        echo "\n";
 
         // Step 2: Handle the comma-separated tables and add dynamic schema to each table (only once)
         $query = preg_replace_callback('/\b(?:FROM|JOIN|LEFT\s+JOIN|RIGHT\s+JOIN)\s+([^,]+)/i', function($matches) use ($schema) {
