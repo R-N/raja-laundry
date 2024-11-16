d0937716d71aaf36da935f50cb2577409ad07311
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-16 17:03:00 +0700

    Revert "Update MY_Loader.php"
    This reverts commit e6950944c76747b6ebbc9eb0a914b22229567897.

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -11,4 +11,45 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class MY_Loader extends HungNG_Loader
 {    /* overloaded methods */
 
+    public function database( $params = '', $return = false, $query_builder = null ) {
+        $ci =& get_instance( );
+
+        if ( $return === false && $query_builder === null && isset( $ci->db ) && is_object( $ci->db ) && !empty( $ci->db->conn_id) ) {
+            return false;
+        }
+
+        require_once( BASEPATH . 'database/DB.php' );
+
+        try{
+            $db =& DB( $params, $query_builder );
+        }catch(Exception $e){
+            if (strpos($e->getMessage(), 'pg_query(): Query failed: ERROR: syntax error at or near "-" LINE 1: SET search_path') !== false){
+            } else {
+                throw $e;
+            }
+        }
+
+        $driver = config_item( 'subclass_prefix' ) . 'DB_' . $db->dbdriver . '_driver';
+        $dirs = array("libraries", "core", "database", "database/drivers");
+        foreach ($dirs as $dir){
+            $file = APPPATH . "{$dir}/{$driver}.php";
+    
+            if ( file_exists( $file ) === true && is_file( $file ) === true ) {
+                require_once( $file );
+    
+                $dbo = new $driver( get_object_vars( $db ) );
+                $db = & $dbo;
+                break;
+            }
+        }
+
+        if ( $return === true ) {
+            return $db;
+        }
+
+        $ci->db = '';
+        $ci->db = $db;
+
+        return $this;
+    }
 }
